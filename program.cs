4d8353c2beeaf0d88ff36db830cff8ce9f0e328f
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2024-02-11 11:23:43 +0700

    add randomString

--- program.cs
+++ program.cs
@@ -2,16 +2,37 @@ using System;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpression;
+using System.Diagnostics;
+using Math.Random;
 
 namespace program{
     class program{
+        private string randomString(){
+            var numchars = "0123456789ABCDEFGHIJKLMNOPQRSRTUVWXYZ";
+            var stringChars = new char[2];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++){
+                stringChars[i] = numchars[random.Next(numchars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
         private static void main(){
             string[] listChar;
             int bufferCount;
             int matrix_X;
             int matrix_Y;
             string[][] matriksMain;
+            string pattern = @"[0-9A-Z]{2}";
             int sequenceCount;
+            string sequenceCode;
+            bool sequenceEnd;
+            int sum;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
 
             Console.Write("Masukkan Jumlah Buffer yang diinginkan : ");
             bufferCount = Convert.ToInt32(Console.ReadLine());
